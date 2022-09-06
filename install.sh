1823e146793f2b9e1b6027dc92109c5c65da7418
Christopher Ptak <christopher@ptak.io>
2022-09-06 02:13:40 -0400

    Updated `install.sh` to only install changed files

--- install.sh
+++ install.sh
@@ -2,20 +2,45 @@
 
 set -e
 
-echo 'Installing Zsh configuration'
-cp ".zshrc"                 "$HOME/.zshrc"
-cp ".zshrc.d/00_prompt"     "$HOME/.zshrc.d"
-cp ".zshrc.d/01_history"    "$HOME/.zshrc.d"
-cp ".zshrc.d/02_completion" "$HOME/.zshrc.d"
-cp ".zshrc.d/03_colors"     "$HOME/.zshrc.d"
+install_file ()
+{
+  src_file="$1"
+  dest_file="$2"
 
+  if ! cmp -s "$src_file" "$dest_file"
+  then
+    echo "Copying $src_file -> $dest_file"
+    cp "$src_file" "$dest_file"
+  fi
+}
 
+make_directory ()
+{
+  directory="$1"
+
+  if [ ! -d "$directory" ]
+  then
+    echo "Creating directory $directory"
+    mkdir -p "$directory"
+  fi
+}
+
+# Install vim configuration
+install_file ".vimrc" "$HOME/.vimrc"
+
+# Install zsh configuration
+make_directory "$HOME/.zshrc.d"
+install_file ".zshrc"                 "$HOME/.zshrc"
+install_file ".zshrc.d/00_prompt"     "$HOME/.zshrc.d/00_prompt"
+install_file ".zshrc.d/01_history"    "$HOME/.zshrc.d/01_history"
+install_file ".zshrc.d/02_completion" "$HOME/.zshrc.d/02_completion"
+install_file ".zshrc.d/03_colors"     "$HOME/.zshrc.d/03_colors"
+
+# Update shell
 if [ "$SHELL" != "/bin/zsh" ]
 then
     echo 'Setting shell to /bin/zsh'
     chsh -s /bin/zsh
 fi
 
-echo 'Installing Vim configuration'
-cp ".vimrc" "$HOME/.vimrc"
 
